8deebeb41643dcd00b65fc322e0102211a966e7a
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2022-05-01 00:44:31 +0530

    Create 155.java

---
+++ 155.java
@@ -0,0 +1,36 @@
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    int min = Integer.MAX_VALUE;
+
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        int peek = stack.pop();
+        if (peek == min){
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
