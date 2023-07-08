b2ef970f1868dfbc06858bae980b587a33f8f5ee
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2023-07-08 15:41:49 +0530

    Create 151.java

---
+++ 151.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {        
+        String[] str = s.trim().split("\\s+");
+        
+        String out = "";
+
+        for (int i = str.length - 1; i > 0; i--) {        
+            out += str[i] + " ";
+        }
+
+        return out + str[0];
+    }
+}
