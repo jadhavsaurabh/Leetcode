5f5aa4580000968cd12eb5f3094a7b77be5b300a
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2023-08-06 12:46:11 +0530

    Update 100.java

--- 100.java
+++ 100.java
@@ -14,26 +14,17 @@
  * }
  */
 class Solution {
-    public void inOrder(TreeNode p, TreeNode q, boolean[] res) {
-        if(p != null && q == null) {
-            res[0] = false;
-        }
-        if(q != null && p == null) {
-            res[0] = false;
-        }
-        if(p != null && q != null) {
-            inOrder(p.left, q.left, res);
-            if(p.val != q.val) {
-                res[0] = false;
-            }
-            inOrder(p.right, q.right, res);
-        }
-    }
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        boolean[] res = {true};
-        inOrder(p, q, res);
-        
-        return res[0];
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
