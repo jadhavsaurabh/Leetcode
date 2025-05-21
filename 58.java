e1620f101b0f8569476f6a0a0ab4435b9c021632
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2025-05-22 00:38:24 +0530

    Update 58.java

--- 58.java
+++ 58.java
@@ -6,3 +6,24 @@ class Solution {
         return strings[strings.length - 1].length();
     }
 }
+
+// Optimized
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
