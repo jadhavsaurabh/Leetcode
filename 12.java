40260cc6a71a1edf5df1605829bb4d6f0f2a92f9
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2025-05-22 13:59:23 +0530

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {
+            1000, 900, 500, 400,
+             100,  90,  50,  40,
+              10,   9,   5,   4, 1
+        };
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        StringBuilder roman = new StringBuilder();
+
+        for (int i = 0; i < values.length && num > 0; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman.append(symbols[i]);
+            }
+        }
+
+        return roman.toString();
+    }
+}
