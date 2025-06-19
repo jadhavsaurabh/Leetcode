24c265a4df8769b1914fffebb70770006ed27f47
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2025-06-19 23:39:17 +0530

    Update 199.java

--- 199.java
+++ 199.java
@@ -38,3 +38,34 @@ class Solution {
     }
 
 }
+
+
+
+/* Alternate with one queue, instead of declaring another queue just take size of queue and iterate till the size*/
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) {
+            return new ArrayList<>();
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            int sz = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i=0;i<sz;i++) {
+                TreeNode node = q.poll();
+                if(node != null) {
+                    if(i==(sz-1)) {
+                        res.add(node.val);
+                    }
+                    if(node.left != null) q.add(node.left);
+                    if(node.right != null) q.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+}
