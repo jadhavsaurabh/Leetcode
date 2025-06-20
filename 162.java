ac2f1d40aa898eba48fc966d28e72b77185c5f66
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2025-06-20 12:20:00 +0530

    Create 162.java

---
+++ 162.java
@@ -0,0 +1,18 @@
+// Brute force
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 1) return 0;
+        int res = -1;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+
+        for(int i=1;i<nums.length-1;i++) {
+           if (nums[i - 1] < nums[i] && nums[i] > nums[i+1]) {
+               res = i;
+               break;
+           }
+        }
+
+        return res;
+    }
+}
