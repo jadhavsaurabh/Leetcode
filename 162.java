606265af15988509b9ec35f8aa0a84ab27a14a68
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2025-06-20 13:45:13 +0530

    Update 162.java

--- 162.java
+++ 162.java
@@ -16,3 +16,21 @@ class Solution {
         return res;
     }
 }
+
+// Optimized
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
