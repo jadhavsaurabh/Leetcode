aaae164a57b6453b3da403ce2b1aa076b0d9c459
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2025-08-27 18:56:47 +0530

    Create 846.java

---
+++ 846.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isNStraightHand(int[] nums, int k) {
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+
+        for(int num : nums) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        }
+
+        while(!mp.isEmpty()) {
+            int first = mp.firstKey();
+            
+            for(int i=0;i<k;i++) {
+                int next = first + i;
+                if(mp.containsKey(next)) {
+                    int occ = mp.get(next);
+                    if(occ == 1) {
+                        mp.remove(next);
+                    } else {
+                        mp.put(next, occ - 1);
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
