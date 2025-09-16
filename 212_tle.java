5c74c3c344dbedf61aca15a038bce12a45e6715d
Saurabh Jadhav <31368794+jadhavsaurabh@users.noreply.github.com>
2025-09-16 23:29:00 +0530

    Create 212_tle.java

---
+++ 212_tle.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean dfs(char[][] board, int i, int j, String word, int idx, boolean[][] visited) {
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]) return false;
+        if(word.charAt(idx) == board[i][j] && idx == word.length() - 1) return true;
+        if(word.charAt(idx) != board[i][j]) return false;
+
+        visited[i][j] = true;
+        boolean right = dfs(board, i, j + 1, word, idx + 1, visited);
+        boolean up = dfs(board, i - 1, j, word, idx + 1, visited);
+        boolean left = dfs(board, i , j - 1, word, idx + 1, visited);
+        boolean down = dfs(board, i + 1, j , word, idx + 1, visited);
+
+        visited[i][j] = false;
+        return right || up || left || down;
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        Set<String> res = new HashSet<>();
+        for(String word: words) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    boolean[][] visited = new boolean[board.length][board[0].length];
+                    if (word.charAt(0) == board[i][j] && dfs(board, i, j, word, 0, visited)) {
+                        res.add(word);
+                    }
+                }
+            }
+        }
+
+        return res.stream().toList();
+    }
+}
